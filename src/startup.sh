7deb94a149d7d7749cd703b284d97d39e1218506
sevak <seva.kabrits@gmail.com>
2025-01-13 21:58:40 +0300

    fix logging

--- src/startup.sh
+++ src/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python main.py &
-gunicorn --bind 0.0.0.0:443 --certfile=/certs/tls.crt --keyfile=/certs/tls.key "validator:app"
+gunicorn "validator:app" --bind 0.0.0.0:443 --certfile=/certs/tls.crt --keyfile=/certs/tls.key --capture-output --access-logfile '-' --error-logfile '-'
